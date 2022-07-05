cba1eefd9c6f7ee64d9664858476ff1c1171ec0a
cgconverse <cathleneconverse@gmail.com>
2022-07-05 14:05:57 -0700

    added notes to javascript file

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ window.onload = function () {
     var Interval ;
   
     buttonStart.onclick = function() {
+
+        // The setInterval() method calls a function at specified intervals (in milliseconds).
+
+        // The setInterval() method continues calling the function until clearInterval() is called, or the window is closed.
+        
+        // 1 second = 1000 milliseconds.
       
       clearInterval(Interval);
        Interval = setInterval(startTimer, 10);
